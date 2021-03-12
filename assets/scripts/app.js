c1118ff2a05412b2af064b3e6cedc8e57ecb444e
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-03-12 08:46:20 +0700

    ADD:JS Folder

---
+++ assets/scripts/app.js
@@ -0,0 +1,15 @@
+const defaultResult = 0;
+
+let currentResult = defaultEesult;
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
+
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert("the result is : ", result);
+}
+
+currentResult = add(1, 2);
+
+let calculationDescription = `(${defaultResult} + + 10) * 3/2-1`;
+
+console.log(defaultEesult);
